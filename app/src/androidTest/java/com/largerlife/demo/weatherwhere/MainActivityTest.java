814443192afe28523a3332e8698f33a780726982
laszlo_galosi <laszlo.galosi78@gmail.com>
2015-06-21 10:40:09 +0200

    Basic Google maps interface and setup.

---
+++ app/src/androidTest/java/com/largerlife/demo/weatherwhere/MainActivityTest.java
@@ -0,0 +1,31 @@
+package com.largerlife.demo.weatherwhere;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by LargerLife on 18/06/15.
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public final ActivityRule<MainActivity> main = new ActivityRule<>(MainActivity.class);
+
+    @Test
+    public void shouldBeAbleToLaunchMainScreen() {
+        onView(withId(R.id.fab)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void MyLocationButoon_ShouldShowMyLocation() {
+        onView(withId(R.id.fab)).perform();
+    }
+}
--- app/src/main/java/com/largerlife/demo/weatherwhere/MainActivity.java
+++ app/src/main/java/com/largerlife/demo/weatherwhere/MainActivity.java
@@ -1,17 +1,80 @@
 package com.largerlife.demo.weatherwhere;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
+        GoogleMap.CancelableCallback {
+
+    @InjectView(R.id.mainLayout) CoordinatorLayout mCoordinatorLayout;
+    @InjectView(R.id.fab) FloatingActionButton mFabMyLocation;
+    private boolean mMapReady = false;
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMapReady = true;
+        mMap = googleMap;
+        LatLng targetBudapest = new LatLng(47.4812134, 19.1303031);
+        CameraPosition target = CameraPosition.builder().target(targetBudapest).zoom(10).build();
+
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+
+//        MarkerOptions markerHome = new MarkerOptions()
+//                .position(new LatLng(47.4948, 19.0348))
+//                .title("Home")
+//                .icon(BitmapDescriptorFactory.fromResource(R.drawable.weather_cloudy));
+//        mMap.addMarker(markerHome);
+        onFinish();
+//        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(target), 5000, this);
+    }
+
+    private void makeSnackBar(int messageResId, int actionResId) {
+        Snackbar.make(mCoordinatorLayout,
+                getString(messageResId), Snackbar.LENGTH_LONG)
+                .setAction(getString(actionResId), new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //TODO: snackbar action.
+                    }
+                }).show();
+    }
+
+    @Override
+    public void onFinish() {
+        makeSnackBar(R.string.snack_tap_place, R.string.snack_action_ok);
+    }
+
+    @Override
+    public void onCancel() {
+        makeSnackBar(R.string.snack_tap_place, R.string.snack_action_ok);
     }
 
     @Override
